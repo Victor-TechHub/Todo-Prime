5596ca183c3750c49bfe19285f0f55829728bdaa
Victor Ezekiel <pianistpro8@gmail.com>
2024-06-06 14:22:36 +0100

    env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlWOpiU8WZ_leSzqDNE-LcAaH_DYrLPI4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "todo-app-full-stack.firebaseapp.com",
   projectId: "todo-app-full-stack",
   storageBucket: "todo-app-full-stack.appspot.com",
   messagingSenderId: "523554615144",
   appId: "1:523554615144:web:e8fcf3add17937846ef0dd"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app)
